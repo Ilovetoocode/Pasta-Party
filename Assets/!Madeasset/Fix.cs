32b32a0b69a6798b1b2d422a0d4f9dbfac135fa6
Ilovetoocode <46007308+Ilovetoocode@users.noreply.github.com>
2022-02-04 09:28:26 -0700

    Better

--- Assets/!Madeasset/Fix.cs
+++ Assets/!Madeasset/Fix.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class Fix : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        var agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
